891216755f7dda82339f9d3e4017f97ac20c5e16
Tyler <twwaneka@gmail.com>
2014-04-16 15:31:42 -0700

    skeleton for testing getPetitions
    - requires the WhiteHouse.js file
    - creates an instance of WhiteHouse

--- test/spec.js
+++ test/spec.js
@@ -1,2 +1,12 @@
 var should = require('should')
   , sinon = require('sinon')
+  , wh = require('../lib/WhiteHouse')
+  , whApi = wh.createWhiteHouse()
+
+describe('getPetitions', function() {
+  describe('when called with one argument (cb)', function() {
+    it('should pass the callback to the api call function', function() {
+
+    })
+  })
+})
