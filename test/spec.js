712cc2988ab319d4ec367d1d7a17f3c5699c7ccc
Tyler <twwaneka@gmail.com>
2014-04-16 16:02:50 -0700

    refactor the getSignatures tests
    - beforeEach and afterEach handle the stubbing

--- test/spec.js
+++ test/spec.js
@@ -33,34 +33,29 @@ describe('getPetitions', function() {
 })
 
 describe('getSignatures', function() {
+  var stub
+  beforeEach(function() {
+    stub = sinon.stub(whApi, 'apiCall')
+  })
+  afterEach(function() {
+    whApi.apiCall.restore()
+  })
   describe('when called with two arguments (petition, cb)', function() {
     it('should pass the arguments to the api call function', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getSignatures('fakePetition', 'fakeCallback')
       stub.called.should.be.true
-
-      whApi.apiCall.restore()
     })
   })
   describe('when called with three arguments (petition, params, cb)', function() {
     it('should pass the arguments to the api call function', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getSignatures('fakePetition', 'fakeParams', 'fakeCallback')
       stub.called.should.be.true
-
-      whApi.apiCall.restore()
     })
   })
   describe('when called with the right number of arguments', function() {
     it('should throw an error', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getSignatures.bind(null).should.throw()
       whApi.getSignatures.bind(null, 'fakePetition', 'fakeParams', 'fakeCallback', 'extraArg').should.throw()
-
-      whApi.apiCall.restore()
     })
   })
 })
