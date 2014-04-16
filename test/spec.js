f87083591c2f5c85ba9c9b291f3c4ba67a36448d
Tyler <twwaneka@gmail.com>
2014-04-16 16:02:01 -0700

    refactor getPetition tests
    - beforeEach and afterEach handle the stubbing

--- test/spec.js
+++ test/spec.js
@@ -4,35 +4,30 @@ var should = require('should')
   , whApi = wh.createWhiteHouse()
 
 describe('getPetitions', function() {
+  var stub
+  beforeEach(function() {
+    stub = sinon.stub(whApi, 'apiCall')
+  })
+  afterEach(function() {
+    whApi.apiCall.restore()
+  })
   describe('when called with one argument (cb)', function() {
     it('should pass the callback to the api call function', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getPetitions('fakeCallback')
       stub.called.should.be.true
-
-      whApi.apiCall.restore()
     })
   })
   describe('when called with two arguments (params, cb)', function() {
     it('should pass the arguments to the api call function', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getPetitions('fakeParams', 'fakeCallback')
       stub.called.should.be.true
-
-      whApi.apiCall.restore()
     })
   })
   describe('when called with the wrong number of arguments', function() {
     it('should throw and error', function() {
-      var stub = sinon.stub(whApi, 'apiCall')
-
       whApi.getPetitions.bind(null).should.throw()
       whApi.getPetitions.bind(null, 'fakeParams', 'fakeCallback', 'extraArg').should.throw()
       stub.called.should.be.false
-
-      whApi.apiCall.restore()
     })
   })
 })
